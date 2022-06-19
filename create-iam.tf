dfaad86daa66645719ee20809f1a3cd8ba227a81
naotoy <yamamotonaoto@yamamotonaotos-MacBook-Air.local>
2022-06-19 14:43:54 +0900

    password

--- create-iam.tf
+++ create-iam.tf
@@ -6,3 +6,8 @@ resource "aws_iam_user" "root" {
 resource "aws_iam_access_key" "root" {
   user = aws_iam_user.root.name
 }
+
+resource "aws_iam_user_login_profile" "example" {
+  user    = aws_iam_user.root.name
+  password = "AQWS!test"
+}
