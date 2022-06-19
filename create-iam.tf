44819dbd5e317491244d7811161f6d1b192a377c
naotoy <yamamotonaoto@yamamotonaotos-MacBook-Air.local>
2022-06-19 14:59:28 +0900

    password

--- create-iam.tf
+++ create-iam.tf
@@ -11,3 +11,14 @@ resource "aws_iam_user_login_profile" "admin" {
   user    = aws_iam_user.admin.name
   pgp_key = "test"
 }
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn
+  }
+}
