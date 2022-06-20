31e61c90b48a9fed34cbbb467927cfd73bebba85
naotoy <yamamotonaoto@yamamotonaotos-MacBook-Air.local>
2022-06-20 10:16:16 +0900

    password

--- create-iam.tf
+++ create-iam.tf
@@ -1,21 +1,9 @@
-resource "aws_iam_user" "admin" {
-  name = "admin"
-}
-
-resource "aws_iam_access_key" "admin" {
-  user = aws_iam_user.admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_user_login_profile" "admin" {
-  user    = aws_iam_user.admin.name
-  pgp_key = "test"
-}
 
 resource "aws_lb_listener" "app" {
   load_balancer_arn = aws_lb.app.arn
   port = "80"
-  protocol = "HTTP"
+  protocol = "HTTPS"
+  internal = false
 
   default_action {
     type = "forward"
